c29167bd5d0e3136a1f316cd5865bc4d7338fe29
Stefan Oestreicher <oestef@gmail.com>
2020-06-20 00:06:08 +0200

    Add notification permission warning

--- web/src/MobTimer.js
+++ web/src/MobTimer.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState, useRef } from "react";
 import MobTimerIdle from "./MobTimerIdle";
 import MobTimerRunning from "./MobTimerRunning";
 import * as Mob from "./Mob";
+import * as Icon from "./icons";
 
 const MobTimer = ({ id, mob, onChange, onNotify, timeDelta }) => {
   const [volume, setVolume] = useState(50);
+  const canNotify = Notification.permission === "granted";
   return (
     <div className="flex flex-col h-screen">
+      {!canNotify && (
+        <div className="bg-yellow-300 text-xs flex p-1 cursor-pointer space-x-2">
+          <Icon.AlertTriangle size={18} />
+          <div>Notification permissions are not granted.</div>
+        </div>
+      )}
       <div className="flex space-x-2 text-sm text-gray-700 items-center p-2 justify-end">
         <div>Volume:</div>
         <input
--- web/src/icons.js
+++ web/src/icons.js
@@ -1,9 +1,7 @@
-// export { Trash, Circle, CheckCircle } from "react-feather/build/";
-
 export { default as Trash } from "react-feather/dist/icons/trash";
-export { default as Circle } from "react-feather/dist/icons/circle";
-export { default as CheckCircle } from "react-feather/dist/icons/check-circle";
 
 export { default as Plus } from "react-feather/dist/icons/plus";
 export { default as Minus } from "react-feather/dist/icons/minus";
 export { default as ArrowRight } from "react-feather/dist/icons/arrow-right";
+
+export { default as AlertTriangle } from "react-feather/dist/icons/alert-triangle";
